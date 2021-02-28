cb572ecbc92d52e71aba24ad3da568d63b246542
imran2197 <samimdimran786@gmail.com>
2021-02-28 15:54:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import { Button } from 'reactstrap';
+import connectFour from "./components/connectFour";
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
+import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined';
+import ExtensionOutlinedIcon from '@material-ui/icons/ExtensionOutlined';
+import CopyrightOutlinedIcon from '@material-ui/icons/CopyrightOutlined';
+import PlayCircleFilledWhiteOutlinedIcon from '@material-ui/icons/PlayCircleFilledWhiteOutlined';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <div className="quote-container">
+          <h1 className="name">CONNECT FOUR!</h1>
+          <p className="quote">Play with other players around the world.</p>
+        </div> 
+      </div>
+      <div className="board">
+        <div className="header">
+          <Button className="play"><PlayCircleFilledWhiteOutlinedIcon id="playlogo" />PLAY</Button>
+          <img className="img" src="./connect.png" alt="ConnectFour Logo" /> 
+        </div>
+        <hr />
+          <div className="buttons">
+            <Button className="button" id="custom"><PersonOutlineOutlinedIcon id="icons" />Custom Game<div id="comingSoon">Coming Soon</div></Button>
+            <Button className="button" id="two"><PeopleAltOutlinedIcon id="icons" />Two Players</Button>
+            <Button className="button" id="online"><LanguageOutlinedIcon id="icons" />Game Online<div id="comingSoon">Coming Soon</div></Button>
+            <Button className="button" id="training"><ExtensionOutlinedIcon id="icons" />Training Game<div id="comingSoon">Coming Soon</div></Button>
+          </div>
+      </div>
+      <div className="footer">
+        <CopyrightOutlinedIcon />
+        2020
+      </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/connectFour.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const connectFour = () => {
+    return(
+        <>
+            <h1>Game</h1>
+        </>
+    )
+}
+export default connectFour;
